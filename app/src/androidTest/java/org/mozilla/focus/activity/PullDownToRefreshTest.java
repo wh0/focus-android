136607073f1c33e53351fd5414a406ba473c9db1
isabelrios <isabelrios@gmail.com>
2017-11-21 17:43:53 +0100

    adding a test webpage to check when the page reloads

--- app/src/androidTest/java/org/mozilla/focus/activity/PullDownToRefreshTest.java
+++ app/src/androidTest/java/org/mozilla/focus/activity/PullDownToRefreshTest.java
@@ -15,7 +15,6 @@ import android.view.View;
 
 import org.hamcrest.Matcher;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -47,14 +46,13 @@ import static android.support.test.espresso.web.sugar.Web.onWebView;
 import static android.support.test.espresso.web.webdriver.DriverAtoms.findElement;
 import static android.support.test.espresso.web.webdriver.DriverAtoms.getText;
 import static org.hamcrest.Matchers.containsString;
-import static org.mozilla.focus.activity.TestHelper.webPageLoadwaitingTime;
-import static org.mozilla.focus.activity.helpers.EspressoHelper.navigateToWebsite;
 
 @RunWith(AndroidJUnit4.class)
 public class PullDownToRefreshTest {
-    private static final String MOZILLA_WEBSITE_SLOGAN_SELECTOR = ".content h2";
-    private static final String MOZILLA_WEBSITE_SLOGAN_TEXT = "Internet for people,\nnot profit.";
     private static final String COUNTER = "counter";
+    private static final String FIRST_TIME = "1";
+    private static final String SECOND_TIME = "2";
+
 
     private MockWebServer webServer;
     @Rule
@@ -84,16 +82,8 @@ public class PullDownToRefreshTest {
 
     @Test
     public void pullDownToRefreshTest() throws InterruptedException, UiObjectNotFoundException, IOException {
-        // Go to a website
-        //navigateToWebsite(("file:///Users/irios/Downloads/counter.html"));
 
-        //TestHelper.progressBar.waitForExists(webPageLoadwaitingTime);
-        //Assert.assertTrue(TestHelper.progressBar.exists());
-        //Assert.assertTrue(TestHelper.progressBar.waitUntilGone(webPageLoadwaitingTime));
-
-        // Check that the webview is loaded
-        //assertWebsiteUrlContains("counter.html");
-        onView(withId(R.id.url_edit))
+        onView(withId(R.id.urlView))
                 .check(matches(isDisplayed()))
                 .check(matches(hasFocus()))
                 .perform(click(), replaceText(webServer.url("/").toString()), pressImeActionButton());
@@ -103,8 +93,8 @@ public class PullDownToRefreshTest {
                 .check(matches(withText(containsString(webServer.getHostName()))));
 
         onWebView()
-                .withElement(findElement(Locator.ID, COUNTER));
-                //.check(webMatches(getText(), containsString(MOZILLA_WEBSITE_SLOGAN_TEXT)));
+                .withElement(findElement(Locator.ID, COUNTER))
+                .check(webMatches(getText(), containsString(FIRST_TIME)));
 
         onView(withId(R.id.swipe_refresh))
                 .check(matches(isDisplayed()));
@@ -114,21 +104,11 @@ public class PullDownToRefreshTest {
               .perform(withCustomConstraints(swipeDown(), isDisplayingAtLeast(85)))
               .check(matches(hasChildCount(2)));
 
-        Assert.assertTrue(TestHelper.progressBar.waitForExists(webPageLoadwaitingTime));
-
-        // Check that the webview is loaded again and the progress bar dissapears
-        //onWebView()
-          //     .withElement(findElement(Locator.ID, COUNTER));
-         //       .check(webMatches(getText(), containsString(MOZILLA_WEBSITE_SLOGAN_TEXT)));
-
-       //Assert.assertFalse(TestHelper.progressBar.exists());
+        onWebView()
+                .withElement(findElement(Locator.ID, COUNTER))
+                .check(webMatches(getText(), containsString(SECOND_TIME)));
     }
 
-    private void assertWebsiteUrlContains(String substring) {
-        onView(withId(R.id.display_url))
-                .check(matches(isDisplayed()))
-                .check(matches(withText(containsString(substring))));
-    }
     public static ViewAction withCustomConstraints(final ViewAction action, final Matcher<View> constraints) {
         return new ViewAction() {
             @Override
